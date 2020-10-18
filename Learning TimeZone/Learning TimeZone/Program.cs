bdcb3a65b07a183616e9a679896bfb92f18fd5b0
Creator <gtarasu1993@gmail.com>
2020-10-18 22:03:12 +0530

    Different Time Zone Conversion Learning in C#
    1. EST
    2. IST
    3. UTC

--- Learning TimeZone/Learning TimeZone/Program.cs
+++ Learning TimeZone/Learning TimeZone/Program.cs
@@ -33,7 +33,7 @@ namespace Learning_TimeZone
 			Console.WriteLine();
 			Console.WriteLine();
 
-			Console.WriteLine("Local Time to Est Conversion");
+			Console.WriteLine("Local Time to UTC Conversion");
 			DateTime CreatedDate = new DateTime(2020, 10, 14, 17, 46, 01, 150);
 			Console.WriteLine("CreatedDate = {0}", CreatedDate);
 			Console.WriteLine();
@@ -42,8 +42,46 @@ namespace Learning_TimeZone
 			Console.WriteLine();
 			Console.WriteLine();
 
-			//Created Date to EST Conversion
-			Console.WriteLine("Created Date to EST Conversion");
+
+			//UTC Date to EST Conversion
+			Console.WriteLine("Coordinated Universal Time (UTC) is 4 hours ahead of Eastern Standard Time(EST)");
+			Console.WriteLine();
+			var utcDate = DateTime.UtcNow;
+			Console.WriteLine("UTC to EST Conversion");
+			Console.WriteLine();
+			TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+			DateTime EasternTime = TimeZoneInfo.ConvertTimeFromUtc(utcDate, easternZone);
+			Console.WriteLine("UTC Date {0} into EST Date = {1}", utcDate, EasternTime);
+			Console.WriteLine();
+			Console.WriteLine();
+
+
+
+			//UTC to IST Conversion
+			Console.WriteLine("Indian Standard Time (IST) is 5 hours 30 minutes ahead of Coordinated Universal Time (UTC)");
+			Console.WriteLine();
+			var utcNow = DateTime.UtcNow;
+			Console.WriteLine("UTC to IST Conversion");
+			Console.WriteLine();
+			TimeZoneInfo indianZone = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+			DateTime IndianTime = TimeZoneInfo.ConvertTime(utcNow, indianZone);
+			Console.WriteLine("UTC {0} to IST Date = {1}", utcNow, IndianTime);
+			Console.WriteLine();
+			Console.WriteLine();
+
+
+			//IST to EST Conversion
+			Console.WriteLine("Indian Standard Time(IST) is 9 hours and 30 minutes ahead of Eastern Standard Time(EST)");
+			Console.WriteLine();
+			var nowDate = DateTime.Now;
+			Console.WriteLine("IST to EST Conversion");
+			Console.WriteLine();
+			TimeZoneInfo easternZone1 = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+			DateTime EasternTime1 = TimeZoneInfo.ConvertTime(nowDate, easternZone1);
+			Console.WriteLine("IST Date {0} into EST Date = {1}", nowDate, EasternTime1);
+			Console.WriteLine();
+			Console.WriteLine();
+
 
 			Console.ReadLine();
 		}
